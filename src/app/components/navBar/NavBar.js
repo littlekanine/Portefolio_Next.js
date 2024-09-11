7fd97f7f723543929b5a1d31c44e61b5c6e7553e
littlekanine <elia.kopff3891@gmail.com>
2024-09-11 23:01:02 +0200

    WIP Form / need change background

--- src/app/components/navBar/NavBar.js
+++ src/app/components/navBar/NavBar.js
@@ -15,13 +15,13 @@ const NavBar = ({ darkMode,isNotHome }) => {
                     <Link href="/contactForm" className={`animated-button ${darkMode ? "dark-mode" : ""} ${buttonStyle}`}>
                             <Buttons message={"Contact"} ContactSvg={ContactSvg} SvgLeft={ArrowLeft} darkMode={darkMode} />
                     </Link>
-                    <a rel="noreferrer" href="https://github.com/littlekanine" className={`animated-button ${darkMode ? "dark-mode" : ""}${buttonStyle}`}>
+                    <a rel="noreferrer" href="https://github.com/littlekanine" target="_blank" className={`animated-button ${darkMode ? "dark-mode" : ""}${buttonStyle}`}>
                         <Buttons message={"Github"} GitHubSvg={GitHubSvg} SvgLeft={ArrowLeft} darkMode={darkMode} />
                     </a>
-                    <a rel="noreferrer" href="https://www.linkedin.com/in/elia-kopff-16064b228/?original_referer=https%3A%2F%2Fwww%2Egoogle%2Ecom%2F&originalSubdomain=fr" className={`animated-button ${darkMode ? "dark-mode" : ""}${buttonStyle}`}>
+                    <a rel="noreferrer" href="https://www.linkedin.com/in/elia-kopff-16064b228/?original_referer=https%3A%2F%2Fwww%2Egoogle%2Ecom%2F&originalSubdomain=fr" target="_blank" className={`animated-button ${darkMode ? "dark-mode" : ""}${buttonStyle}`}>
                         <Buttons message={"Linkedin"} LinkedinSvg={LinkedinSvg} SvgLeft={ArrowLeft} darkMode={darkMode} />
                     </a>
-                    <a rel="noreferrer" href="https://x.com/K_D3828" className={`animated-button ${darkMode ? "dark-mode" : ""}${buttonStyle}`}>
+                    <a rel="noreferrer" href="https://x.com/K_D3828" target="_blank" className={`animated-button ${darkMode ? "dark-mode" : ""}${buttonStyle}`}>
                         <Buttons message={"Twitter"} TwitterSvg={TwitterSvg} SvgLeft={ArrowLeft} darkMode={darkMode} />
                     </a>
                 </div>
--- src/app/contactForm/page.js
+++ src/app/contactForm/page.js
@@ -1,19 +1,28 @@
-import Buttons from '../components/buttons/index';
-import { ArrowRight, Enveloppe } from '../components/svgcomponent/SvgComponent';
+import Buttons from "../components/buttons/index";
+import { ArrowRight, Enveloppe } from "../components/svgcomponent/SvgComponent";
 
 function ContactForm() {
-	return (
-		<div>
-			<section id="contact" className="form">
-				<h1 className="section-title">Contact</h1>
-				<div className="form-button hero-cta load-hidden">
-					<a rel="noreferrer" className="animated-button" href="mailto:elia.kopff3891@gmail.com">
-						<Buttons className="animated-button" message={'If you would like to contact me'} SvgRight={ArrowRight} SvgEnvellope={Enveloppe} />
-					</a>
-				</div>
-			</section>
-		</div>
-	);
+    return (
+        <div className="container">
+            <form action="/submit-contact" method="post">
+                <p>Welcome</p>
+                <div className="inputForm flex center">
+                    <input type="name" placeholder="Name" />
+                    <input type="Email" placeholder="Email" />
+                </div>
+                <br />
+                <textarea id="story" name="story" rows="5" cols="33"></textarea>
+            </form>
+
+            <div className="drops">
+                <div className="drop drop-1"></div>
+                <div className="drop drop-2"></div>
+                <div className="drop drop-3"></div>
+                <div className="drop drop-4"></div>
+                <div className="drop drop-5"></div>
+            </div>
+        </div>
+    );
 }
 
 export default ContactForm;
