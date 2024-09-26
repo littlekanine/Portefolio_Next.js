f983373602ecda8f64d7d774cf6a7e815428b2c7
littlekanine <elia.kopff3891@gmail.com>
2024-09-26 12:02:30 +0200

    form ok ! Response ok !

--- src/app/components/buttons/index.js
+++ src/app/components/buttons/index.js
@@ -1,22 +1,22 @@
-import React from "react";
+import React from 'react';
 
-function Buttons({ message, SvgLeft, SvgRight, SvgEnvellope, SvgSun, SvgMoon, isToggle, darkMode, ContactSvg, FolderSvg, GitHubSvg, LinkedinSvg, TwitterSvg }) {
-    return (
-        <div className="flex center align-center">
-            {SvgLeft && <SvgLeft className="arr-2" />}
-            <span className={`text ${isToggle ? "hidden" : ""}`}>{message}</span>
-            <span className={`circle ${isToggle ? "hidden" : ""}`}></span>
-            {SvgEnvellope && <SvgEnvellope className="enveloppe" />}
-            {SvgRight && <SvgRight className={`arr-1 ${darkMode ? "dark-mode" : ""}`} />}
-            {SvgMoon && <SvgMoon className="moon flex center align-center" />}
-            {SvgSun && <SvgSun className="sun" />}
-            {ContactSvg && <ContactSvg className="contactSvg" />}
-            {FolderSvg && <FolderSvg className="folderSvg" />}
-            {GitHubSvg && <GitHubSvg className="gitHubSvh" />}
-            {LinkedinSvg && <LinkedinSvg className="linkedinSvg" />}
-            {TwitterSvg && <TwitterSvg className="twitterSvg" />}
-        </div>
-    );
+function Buttons({ type = 'button', message, SvgLeft, SvgRight, SvgEnvellope, SvgSun, SvgMoon, isToggle, darkMode, ContactSvg, FolderSvg, GitHubSvg, LinkedinSvg, TwitterSvg }) {
+	return (
+		<div className="flex center align-center">
+			{SvgLeft && <SvgLeft className="arr-2" />}
+			<span className={`text ${isToggle ? 'hidden' : ''}`}>{message}</span>
+			<span className={`circle ${isToggle ? 'hidden' : ''}`}></span>
+			{SvgEnvellope && <SvgEnvellope className="enveloppe" />}
+			{SvgRight && <SvgRight className={`arr-1 ${darkMode ? 'dark-mode' : ''}`} />}
+			{SvgMoon && <SvgMoon className="moon flex center align-center" />}
+			{SvgSun && <SvgSun className="sun" />}
+			{ContactSvg && <ContactSvg className="contactSvg" />}
+			{FolderSvg && <FolderSvg className="folderSvg" />}
+			{GitHubSvg && <GitHubSvg className="gitHubSvh" />}
+			{LinkedinSvg && <LinkedinSvg className="linkedinSvg" />}
+			{TwitterSvg && <TwitterSvg className="twitterSvg" />}
+		</div>
+	);
 }
 
 export default Buttons;
--- src/app/components/contactForm/ContactForm.js
+++ src/app/components/contactForm/ContactForm.js
@@ -1,7 +1,91 @@
+import { useState } from 'react';
 import NavBar from '../navBar/NavBar';
-import { Moon, Sun } from '../svgcomponent/SvgComponent.js';
+import { Moon, Sun, Enveloppe } from '../svgcomponent/SvgComponent.js';
+import emailjs from 'emailjs-com';
 
 function ContactForm({ darkMode, handleNavClick, visibleSection, toggleDarkMode }) {
+	const [formData, setFormData] = useState({
+		name: '',
+		email: '',
+		story: '',
+	});
+
+	const [errors, setErrors] = useState({});
+	const [emailSent, setEmailSent] = useState(false);
+
+	const handleInputChange = (e) => {
+		const { name, value } = e.target;
+		setFormData({
+			...formData,
+			[name]: value,
+		});
+	};
+
+	const validateForm = () => {
+		let formErrors = {};
+		if (!formData.name) formErrors.name = true;
+		if (!formData.email) formErrors.email = true;
+		if (!/\S+@\S+\.\S+/.test(formData.email)) formErrors.email = true;
+		if (!formData.story) formErrors.story = true;
+		return formErrors;
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		const formErrors = validateForm();
+		if (Object.keys(formErrors).length === 0) {
+			// No validation errors, send email
+			sendEmail();
+		} else {
+			setErrors(formErrors);
+		}
+	};
+
+	const sendAutoReply = () => {
+		const serviceID = 'service_7td6bhk'; // ID du service
+		const autoReplyTemplateID = 'template_gkufw79'; // ID de ton modèle de réponse
+		const userID = 'n8d9wYxMy30Osyll5'; // ID de l'utilisateur
+
+		const autoReplyParams = {
+			name: formData.name,
+			story: formData.story,
+			to_email: formData.email, // Adresse email de l'expéditeur
+		};
+
+		// Envoi de la réponse automatique
+		emailjs.send(serviceID, autoReplyTemplateID, autoReplyParams, userID).then(
+			(response) => {
+				console.log('Auto-reply sent successfully!', response.status, response.text);
+			},
+			(error) => {
+				console.error('Failed to send auto-reply:', error);
+			}
+		);
+	};
+
+	const sendEmail = () => {
+		const serviceID = 'service_7td6bhk';
+		const templateID = 'template_1xy7vsy';
+		const userID = 'n8d9wYxMy30Osyll5';
+
+		const templateParams = {
+			name: formData.name,
+			email: formData.email,
+			story: formData.story,
+		};
+
+		emailjs.send(serviceID, templateID, templateParams, userID).then(
+			(response) => {
+				console.log('Email sent successfully!', response.status, response.text);
+				setEmailSent(true);
+				sendAutoReply();
+			},
+			(error) => {
+				console.error('Failed to send email:', error);
+			}
+		);
+	};
+
 	return (
 		<div className="containerForm">
 			<NavBar darkMode={darkMode} handleNavClick={handleNavClick} visibleSection={visibleSection} />
@@ -11,14 +95,50 @@ function ContactForm({ darkMode, handleNavClick, visibleSection, toggleDarkMode 
 				</button>
 			</div>
 			<div className="flex center align-center height80vh">
-				<form action="/submit-contact" method="post" className="flex center align-center column">
+				<form onSubmit={handleSubmit} className="flex center align-center column">
 					<p>Welcome</p>
-					<div className="inputForm flex center">
-						<input type="name" placeholder="Name" />
-						<input type="Email" placeholder="Email" />
+					<div className="inputForm flex center column">
+						<div className="flex center align-center row">
+							<label htmlFor="name">Name</label>
+							<input
+								type="text"
+								name="name"
+								placeholder="Name"
+								value={formData.name}
+								onChange={handleInputChange}
+								className={errors.name ? 'input-error' : ''}
+							/>
+						</div>
+						<div className="flex center align-center row">
+							<label htmlFor="email">Email</label>
+							<input
+								type="email"
+								name="email"
+								placeholder="Email"
+								value={formData.email}
+								onChange={handleInputChange}
+								className={errors.email ? 'input-error' : ''}
+							/>
+						</div>
 					</div>
 					<br />
-					<textarea id="story" name="story" rows="5" cols="33"></textarea>
+					<div className="flex center align-center column">
+						<label htmlFor="story">Message</label>
+						<textarea
+							id="story"
+							name="story"
+							rows="5"
+							cols="33"
+							className="flex center align-center"
+							value={formData.story}
+							onChange={handleInputChange}
+						></textarea>
+						{errors.story && <p className="error">{errors.story}</p>}
+					</div>
+					<button type="submit" Enveloppe={Enveloppe} className="animated-button-submit">
+						Submit
+					</button>
+					{emailSent && <p className="success-message">Your message has been sent successfully!</p>}
 				</form>
 			</div>
 
