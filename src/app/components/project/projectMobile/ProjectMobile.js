5eb9998794676a17a67f9efe3397bb77458be3e5
littlekanine <elia.kopff3891@gmail.com>
2024-10-25 23:22:56 +0200

    WIP

--- src/app/components/project/projectMobile/ProjectMobile.js
+++ src/app/components/project/projectMobile/ProjectMobile.js
@@ -1,77 +1,68 @@
-'use client';
+"use client";
 
-import React, { useState, useEffect } from 'react';
-import ProjectsData from '../../../data/projects.json';
-import Modal from './modalMobile/ModalMobile';
+import React, { useState, useEffect } from "react";
+import ProjectsData from "../../../data/projects.json";
+import Modal from "./modalMobile/ModalMobile";
 
 function ProjectsMobile({ darkMode }) {
-	const [selectedProject, setSelectedProject] = useState(null);
-	const [modalOpen, setModalOpen] = useState(false);
-	const [currentIndex, setCurrentIndex] = useState(0);
+    const [selectedProject, setSelectedProject] = useState(null);
+    const [modalOpen, setModalOpen] = useState(false);
+    const [currentIndex, setCurrentIndex] = useState(0);
 
-	const openModal = (project) => {
-		setSelectedProject(project);
-		setModalOpen(true);
-	};
+    const openModal = (project) => {
+        setSelectedProject(project);
+        setModalOpen(true);
+    };
 
-	const closeModal = () => {
-		setModalOpen(false);
-	};
+    const closeModal = () => {
+        setModalOpen(false);
+    };
 
-	const handleNext = () => {
-		setCurrentIndex((prevIndex) => (prevIndex + 1) % ProjectsData.length);
-	};
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrentIndex((prevIndex) => (prevIndex === ProjectsData.length - 1 ? 0 : prevIndex + 1));
+        }, 5000);
 
-	const handlePrev = () => {
-		setCurrentIndex((prevIndex) => (prevIndex === 0 ? ProjectsData.length - 1 : prevIndex - 1));
-	};
+        return () => clearInterval(interval);
+    }, []);
 
-	useEffect(() => {
-		const handleResize = () => {};
-		window.addEventListener('resize', handleResize);
+    useEffect(() => {}, [darkMode]);
 
-		return () => {
-			window.removeEventListener('resize', handleResize);
-		};
-	}, []);
+    return (
+        <div className={`${darkMode ? "dark-mode" : ""} flex column center align-center height100vh`}>
+            <h2 className="section-title section-title-project dark-blue-text section-title-project-mobile">Projects Openclassroom 2023 - 2024</h2>
 
-	useEffect(() => {}, [darkMode]);
-
-	return (
-		<div className={`${darkMode ? 'dark-mode' : ''} flex column center align-center height100vh`}>
-			<h2 className="section-title section-title-project dark-blue-text section-title-project-mobile">Projects Openclassroom 2023 - 2024</h2>
-
-			<div className="carousel-container flex center align-center">
-				<button className="carousel-control prev" onClick={handlePrev}>
-					&lt;
-				</button>
-
-				<div className="carousel-item flex" onClick={() => openModal(ProjectsData[currentIndex])}>
-					<img
-						className="carousel-img flex center align-center"
-						src={ProjectsData[currentIndex].img}
-						alt={ProjectsData[currentIndex].nom}
-						style={{
-							width: '100%',
-							height: '50%',
-							objectFit: 'contain',
-						}}
+            <div className="carousel-container flex center align-center column">
+                <div className="carousel-item flex center align-center" onClick={() => openModal(ProjectsData[currentIndex])}>
+                    <img
+                        className="carousel-img flex center align-center"
+                        src={ProjectsData[currentIndex].img}
+                        alt={ProjectsData[currentIndex].nom}
+                        style={{
+                            width: "80%",
+                            height: "100%",
+                            objectFit: "cover",
+                        }}
+                    />
+                </div>
+                <div className="dots-container flex">
+				{ProjectsData.map((_, index) => (
+					<span
+						key={index}
+						className={`dot ${index === currentIndex ? 'active' : ''}`}
+						onClick={() => setCurrentIndex(index)}
 					/>
-					<p></p>
-				</div>
-
-				<button className="carousel-control next" onClick={handleNext}>
-					&gt;
-				</button>
+				))}
 			</div>
+            </div>
 
-			{modalOpen && (
-				<div className="modal-background">
-					<Modal project={selectedProject} closeModal={closeModal} />
-				</div>
-			)}
-		</div>
-	);
+            {modalOpen && (
+                <div className="modal-background">
+                    <Modal project={selectedProject} closeModal={closeModal} />
+                </div>
+            )}
+        </div>
+    );
 }
 
 export default ProjectsMobile;
